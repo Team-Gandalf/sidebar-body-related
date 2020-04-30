8a9e9046c354ec85a4129155e81469beebe4ede0
John Doe <sfshaider@gmail.com>
2020-04-30 01:06:37 -0400

    webpack configured

---
+++ client/App.jsx
@@ -0,0 +1,21 @@
+class App extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+
+      };
+    }
+  
+  
+    render() {
+      return (
+        <div>
+          MY REACT IS ALIVE! AND IS WORKING!
+        </div>
+      );
+    }
+  }
+
+
+  ReactDOM.render(<App />, document.getElementById("haider-steamy"));
+  
