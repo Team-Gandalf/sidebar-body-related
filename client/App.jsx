e2dc1c377e81e14d97b308871ae5e775a9d28e97
John Doe <sfshaider@gmail.com>
2020-05-01 21:59:59 -0400

    seed script completed and tested

--- client/App.jsx
+++ client/App.jsx
@@ -9,13 +9,45 @@ class App extends React.Component {
   
     render() {
       return (
+      <div>
+        <div class="game_area_details_specs">
         <div>
-          MY REACT IS ALIVE! AND IS WORKING!
+        <a><img src="https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_singlePlayer.png"></img></a>
+        <a class="name" href="https://www.google.com">Single-player</a>
         </div>
-      );
+        <div>
+        <a><img src="https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_achievements.png"></img></a>
+        <a class="name" href="https://www.google.com">Steam Achievements</a>
+        </div>
+        <div>
+        <a><img src="https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_cc.png"></img></a>
+        <a class="name" href="https://www.google.com">Captions Available</a>
+        </div>
+        <div>
+        <a><img src="https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_workshop.png"></img></a>
+        <a class="name" href="https://www.google.com">Steam Workshop</a>
+        </div>
+        <div>
+        <a><img src="https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_editor.png"></img></a>
+        <a class="name" href="https://www.google.com">Includes level editor</a>
+        </div>
+        </div>
+        <div class="block_title vrsupport">
+              VR Support
+        <br>
+        </div>
+        <div class="details_block vrsupport">
+          <div>Headsets</div>
+          </div>
+          <div class="game_area_details_specs">
+            <div class="icon"><a href="https://store.steampowered.com/search/?vrsupport=105"><img class="category_icon" src="https://store.steampowered.com/public/images/v6/ico/ico_vr_headset_valve_index.png"></a></div>
+              <a class="name" href="https://store.steampowered.com/search/?vrsupport=105">Valve Index</a></div><div class="game_area_details_specs"><div class="icon"><a href="https://store.steampowered.com/search/?vrsupport=101"><img class="category_icon" src="https://store.steampowered.com/public/images/v6/ico/ico_vr_headset_htc.png"></a></div><a class="name" href="https://store.steampowered.com/search/?vrsupport=101">HTC Vive</a></div><div class="game_area_details_specs"><div class="icon"><a href="https://store.steampowered.com/search/?vrsupport=102"><img class="category_icon" src="https://store.steampowered.com/public/images/v6/ico/ico_vr_headset_oculus.png"></a></div><a class="name" href="https://store.steampowered.com/search/?vrsupport=102">Oculus Rift</a></div><div class="game_area_details_specs"><div class="icon"><a href="https://store.steampowered.com/search/?vrsupport=104"><img class="category_icon" src="https://store.steampowered.com/public/images/v6/ico/ico_vr_headset_windowsmr.png"></a></div><a class="name" href="https://store.steampowered.com/search/?vrsupport=104">Windows Mixed Reality</a></div><div class="details_block vrsupport"><div>Input</div></div><div class="game_area_details_specs"><div class="icon"><a href="https://store.steampowered.com/search/?vrsupport=201"><img class="category_icon" src="https://store.steampowered.com/public/images/v6/ico/ico_vr_input_motion.png"></a></div><a class="name" href="https://store.steampowered.com/search/?vrsupport=201">Tracked Motion Controllers</a></div><div class="details_block vrsupport"><div>Play Area</div></div><div class="game_area_details_specs"><div class="icon"><a href="https://store.steampowered.com/search/?vrsupport=301"><img class="category_icon" src="https://store.steampowered.com/public/images/v6/ico/ico_vr_area_seated.png"></a></div><a class="name" href="https://store.steampowered.com/search/?vrsupport=301">Seated</a></div><div class="game_area_details_specs"><div class="icon"><a href="https://store.steampowered.com/search/?vrsupport=302"><img class="category_icon" src="https://store.steampowered.com/public/images/v6/ico/ico_vr_area_standing.png"></a></div><a class="name" href="https://store.steampowered.com/search/?vrsupport=302">Standing</a></div><div class="game_area_details_specs"><div class="icon"><a href="https://store.steampowered.com/search/?vrsupport=303"><img class="category_icon" src="https://store.steampowered.com/public/images/v6/ico/ico_vr_area_roomscale.png"></a></div><a class="name" href="https://store.steampowered.com/search/?vrsupport=303">Room-Scale</a></div>						</div>
+      </div>
+      )
     }
-  }
+};
 
 
   ReactDOM.render(<App />, document.getElementById("haider-steamy"));
-  
+
+
--- server/index.js
+++ server/index.js
@@ -1,29 +1,26 @@
 const express = require("express");
 const path = require("path");
 const models = require("../database/models.js")
-
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.json());
+
 
 var hostname = 'localhost';
 var port = 1991;
 
-app.get("/", (req, res) => {
-  models.saveInfo();
-  models.saveRelated();
-  res.send("info and related generated")
-  res.sendStatus(200);
-  res.end()
-})
-
 app.get("/mainbody", (req, res) => {
-    models.getAll( (err, data) => {
-      if (err) {
-        res.status(404).send(err);
-      } else {
-        res.status(200).send(data);
-      }
-    })
+  let query = req.body.name
+  models.getOne(query, (err, data) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).send(data);
+    }
   })
+})
 
 app.get("/related-content", (req, res) => {
   models.getRelatedContent( (err, data) => {
