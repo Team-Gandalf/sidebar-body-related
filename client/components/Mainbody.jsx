f35d88213cd4741276725d46f42c7e6241a860e1
John Doe <sfshaider@gmail.com>
2020-05-07 23:49:44 -0400

    css work

--- client/components/Mainbody.jsx
+++ client/components/Mainbody.jsx
@@ -13,27 +13,26 @@ const gphx = props.mainbodydata.sysrequirement.graphics;
     return (
       <div className="leftcolumn">
           <h2>ABOUT THIS GAME:</h2> 
-            <div id="mainbodycontent">{description}</div> 
-            <img src={props.mainbodydata.images[0]} width="50%" height="50%"/>
-            <div id="mainbodycontent">{description}</div> 
-            <img src={props.mainbodydata.images[1]} width="50%" height="50%"/>
-            <div id="mainbodycontent">{description}</div>
+            <div id="mainbodytext">{description}</div> 
+            <img id="mainbodyimage" src={props.mainbodydata.images[0]} width="50%" height="50%"/>
+            <div id="mainbodytext">{description}</div> 
+            <img id="mainbodyimage" src={props.mainbodydata.images[1]} width="50%" height="50%"/>
+            <div id="mainbodytext">{description}</div>
           <br></br>
           <strong>ABOUT GAMEPLAY IN VR:</strong> 
-            <div>{description}</div> 
+            <div id="mainbodytext">{description}</div> 
           <br></br>
           <h2>MATURE CONTENT DESCRIPTION:</h2>
-            <div>The developers describe the content like this: <div>
-            <div>{matureContent}</div>
+            <p>The developers describe the content like this: </p>
+            <i>{matureContent}</i>
+          <br></br>
           <br></br>
           <h2>SYSTEM REQUIREMENTS:</h2>
-          <div>Minimum: </div>
-            <div id="key">OS: <span>{os}</span></div>
-            <div id="key">Processor: {processor}</div>
-            <div id="key">Memory: {memory}</div>
-            <div id="key">Graphics: {gphx}</div>
-          </div>
-        </div>
+            <div id="sysmin"> MINIMUM: </div> <br></br>
+            <div id="sysspec">OS: <div id="sysmin">{os}</div></div> <br></br>
+            <div id="sysspec">Processor: <span id="sysmin">{processor}</span></div> <br></br>
+            <div id="sysspec">Memory: <span id="sysmin">{memory}</span></div> <br></br>
+            <div id="sysspec">Graphics: <span id="sysmin">{gphx}</span></div> <br></br>
         <RelatedContent relatedcontent={props.relatedcontent}/>           
       </div>
     )
--- client/components/sub-components/Languages.jsx
+++ client/components/sub-components/Languages.jsx
@@ -3,75 +3,75 @@ import React from 'react';
 const Languages = (props) => {
     console.log("languages props: ", props)
       return (
-          <div className="sidebardesc">
-            <div>Languages: </div>
+          <div className="sidebaritem">
+            <div id="sidebartitle">Languages: </div>
                 <table>
                   <tbody>
                     <tr>
                       <th></th>
-                      <th>Interface</th>
-                      <th>Full Audio</th>
-                      <th>Subtitles</th>
+                      <th id="columntitle">Interface</th>
+                      <th id="columntitle">Full Audio</th>
+                      <th id="columntitle">Subtitles</th>
                     </tr>
                     <tr>
-                      <td>English</td>
-                      <td>{props.languages.English[0] ? "✔" : null}</td>
-                      <td>{props.languages.English[1] ? "✔" : null}</td>
-                      <td>{props.languages.English[2] ? "✔" : null}</td>
+                      <td id="sidebartitle">English</td>
+                      <td id="languagescheckmark">{props.languages.English[0] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.English[1] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.English[2] ? "✔" : null}</td>
                     </tr>
                     <tr>
-                      <td>French</td>
-                      <td>{props.languages.French[0] ? "✔" : null}</td>
-                      <td>{props.languages.French[1] ? "✔" : null}</td>
-                      <td>{props.languages.French[2] ? "✔" : null}</td>
+                      <td id="sidebartitle">French</td>
+                      <td id="languagescheckmark">{props.languages.French[0] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.French[1] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.French[2] ? "✔" : null}</td>
                     </tr>
                     <tr>
-                      <td>German</td>
-                      <td>{props.languages.German[0] ? "✔" : null}</td>
-                      <td>{props.languages.German[1] ? "✔" : null}</td>
-                      <td>{props.languages.German[2] ? "✔" : null}</td>
+                      <td id="sidebartitle">German</td>
+                      <td id="languagescheckmark">{props.languages.German[0] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.German[1] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.German[2] ? "✔" : null}</td>
                     </tr>
                     <tr>
-                      <td>Japanese</td>
-                      <td>{props.languages.Japanese[0] ? "✔" : null}</td>
-                      <td>{props.languages.Japanese[1] ? "✔" : null}</td>
-                      <td>{props.languages.Japanese[2] ? "✔" : null}</td>
+                      <td id="sidebartitle">Japanese</td>
+                      <td id="languagescheckmark">{props.languages.Japanese[0] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.Japanese[1] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.Japanese[2] ? "✔" : null}</td>
                     </tr>
                     <tr>
-                      <td>Korean</td>
-                      <td>{props.languages.Korean[0] ? "✔" : null}</td>
-                      <td>{props.languages.Korean[1] ? "✔" : null}</td>
-                      <td>{props.languages.Korean[2] ? "✔" : null}</td>
+                      <td id="sidebartitle">Korean</td>
+                      <td id="languagescheckmark">{props.languages.Korean[0] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.Korean[1] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.Korean[2] ? "✔" : null}</td>
                     </tr>
                     <tr>
-                      <td>Russian</td>
-                      <td>{props.languages.Russian[0] ? "✔" : null}</td>
-                      <td>{props.languages.Russian[1] ? "✔" : null}</td>
-                      <td>{props.languages.Russian[2] ? "✔" : null}</td>
+                      <td id="sidebartitle">Russian</td>
+                      <td id="languagescheckmark">{props.languages.Russian[0] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.Russian[1] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.Russian[2] ? "✔" : null}</td>
                     </tr>
                     <tr>
-                      <td>Simplified-Chinese</td>
-                      <td>{props.languages.SimplifiedChinese[0] ? "✔" : null}</td>
-                      <td>{props.languages.SimplifiedChinese[1] ? "✔" : null}</td>
-                      <td>{props.languages.SimplifiedChinese[2] ? "✔" : null}</td>
+                      <td id="sidebartitle">Simplified-Chinese</td>
+                      <td id="languagescheckmark">{props.languages.SimplifiedChinese[0] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.SimplifiedChinese[1] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.SimplifiedChinese[2] ? "✔" : null}</td>
                     </tr>
                     <tr>
-                      <td>Spanish-Latin America</td>
-                      <td>{props.languages.SpanishSpain[0] ? "✔" : null}</td>
-                      <td>{props.languages.SpanishSpain[1] ? "✔" : null}</td>
-                      <td>{props.languages.SpanishSpain[2] ? "✔" : null}</td>
+                      <td id="sidebartitle">Spanish-Latin America</td>
+                      <td id="languagescheckmark">{props.languages.SpanishSpain[0] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.SpanishSpain[1] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.SpanishSpain[2] ? "✔" : null}</td>
                     </tr>
                     <tr>
-                      <td>Spanish-Spain</td>
-                      <td>{props.languages.SpanishSpain[0] ? "✔" : null}</td>
-                      <td>{props.languages.SpanishSpain[1] ? "✔" : null}</td>
-                      <td>{props.languages.SpanishSpain[2] ? "✔" : null}</td>
+                      <td id="sidebartitle">Spanish-Spain</td>
+                      <td id="languagescheckmark">{props.languages.SpanishSpain[0] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.SpanishSpain[1] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.SpanishSpain[2] ? "✔" : null}</td>
                     </tr>
                     <tr>
-                      <td>Traditional-Chinese</td>
-                      <td>{props.languages.TraditionalChinese[0] ? "✔" : null}</td>
-                      <td>{props.languages.TraditionalChinese[1] ? "✔" : null}</td>
-                      <td>{props.languages.TraditionalChinese[2] ? "✔" : null}</td>
+                      <td id="sidebartitle">Traditional-Chinese</td>
+                      <td id="languagescheckmark">{props.languages.TraditionalChinese[0] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.TraditionalChinese[1] ? "✔" : null}</td>
+                      <td id="languagescheckmark">{props.languages.TraditionalChinese[2] ? "✔" : null}</td>
                     </tr>
                   </tbody>
                 </table>
