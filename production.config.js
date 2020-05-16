29d7822bf434fbb893d50c7a4bf92bba1f9cb12e
John Doe <sfshaider@gmail.com>
2020-05-16 01:22:02 -0400

    more tweaks

--- production.config.js
+++ production.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -24,6 +24,47 @@ module.exports = {
             presets: ["@babel/preset-react", "@babel/preset-env"],
           },
         },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              query: {
+                name: "assets/[name].[ext]",
+              },
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              query: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                gifsicle: {
+                  interlaced: true,
+                },
+                optipng: {
+                  optimizationLevel: 7,
+                },
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&minetype=application/font-woff",
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader",
       },
     ],
   },
